3a5bf25241dd65eb51537fcee4e7f8c749339495
samuel-018 <sammy.njihiambuthia02@gmail.com>
2026-03-31 14:54:04 +0300

    calculator
    this is a python program to generate a calculator model

---
+++ calculator.py
@@ -0,0 +1,11 @@
+num1 = float(input("enter first number."))
+num2 = float(input("enter second number."))
+
+print("addition.", num1 + num2)
+print("subtraction.", num1 - num2)
+print("multiplication.", num1 * num2)
+
+if num2 != 0:
+    print("division.", num1/num2)
+else:
+      print("division not possible")
